ff669b8fb44bf420f407530915eafe8f3d15b597
Víctor Cañizares <victor.canizares.mata@gmail.com>
2015-02-02 22:22:47 +0100

    #12 Corregido literal en la tabla de niveles de proteccion en CLM

--- scripts/db/Accipiter/update_20150130_increase_size_observaciones_field.sql
+++ scripts/db/Accipiter/update_20150130_increase_size_observaciones_field.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `anuario_bbdd`.`%prefix%cita`
+ALTER TABLE `%prefix%cita`
 CHANGE COLUMN `observaciones` `observaciones` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT 'Observaciones sobre la cita' ;
 
-ALTER TABLE `anuario_bbdd`.`%prefix%cita_historico`
+ALTER TABLE `%prefix%cita_historico`
 CHANGE COLUMN `observaciones` `observaciones` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL COMMENT 'Observaciones sobre la cita' ;
---
+++ scripts/db/Accipiter/update_20150202_2205_corregir_texto_proteccion_clm.sql
@@ -0,0 +1,1 @@
+UPDATE `%prefix%proteccion_clm` SET `nombre`='En Peligro de Extinción' WHERE `codigo`='EN';
