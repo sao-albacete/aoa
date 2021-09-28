37f9db452b30ca8902d9a684ac15fb9a28f61c7a
Víctor Cañizares <victorcm@continuumsecurity.net>
2021-09-28 18:00:46 +0200

    [AOA-130] Corregidos prefijos de tablas en scripts de BD

--- scripts/db/changes/rollback_20210201_0000_update_coordenadas_lugar_int_to_decimal.sql
+++ scripts/db/changes/rollback_20210201_0000_update_coordenadas_lugar_int_to_decimal.sql
@@ -1,9 +1,9 @@
 
 -- Lugar volviendo a integer
-UPDATE cowctkq_lugar SET  lng = NULL WHERE true;
+UPDATE %prefix%_lugar SET  lng = NULL WHERE true;
 ALTER TABLE `%prefix%_lugar`
 CHANGE COLUMN `lng` `coordenadaX` INT  COMMENT 'Coordenada X UTM' ;
 
-UPDATE cowctkq_lugar SET  lat = NULL WHERE true;
+UPDATE %prefix%_lugar SET  lat = NULL WHERE true;
 ALTER TABLE `%prefix%_lugar`
 CHANGE COLUMN `lat` `coordenadaY` INT COMMENT 'Coordenada Y UTM' ;
--- scripts/db/changes/update_20210201_0000_update_coordenadas_lugar_int_to_decimal.sql
+++ scripts/db/changes/update_20210201_0000_update_coordenadas_lugar_int_to_decimal.sql
@@ -1,8 +1,8 @@
 -- Lugar
-UPDATE cowctkq_lugar SET  coordenadaX = NULL WHERE true;
+UPDATE %prefix%_lugar SET  coordenadaX = NULL WHERE true;
 ALTER TABLE `%prefix%_lugar`
 CHANGE COLUMN `coordenadaX` `lng` DECIMAL(11, 8)  SIGNED COMMENT 'Longitud del lugar' ;
 
-UPDATE cowctkq_lugar SET  coordenadaY = NULL WHERE true;
+UPDATE %prefix%_lugar SET  coordenadaY = NULL WHERE true;
 ALTER TABLE `%prefix%_lugar`
 CHANGE COLUMN `coordenadaY` `lat` DECIMAL(10, 8)  SIGNED COMMENT 'Latitud del lugar' ;
